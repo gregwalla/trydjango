5b362548d961cbb05b88f68df4dc910b7908cdea
greg <gregorwa@gmail.com>
2021-11-14 19:44:43 +0100

    two hours elevenmin

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(f"args: {args}, kargs: {kwargs}")
-    print(f"user: {request.user}")
-    #return HttpResponse("<h1>Hello World !</h1>")
+
     return render(request, "home.html", {})
 
 def about_view(request, *args, **kwargs):
-    print(f"args: {args}, kargs: {kwargs}")
-    print(f"user: {request.user}")
-    #return HttpResponse("<h1>Hello World !</h1>")
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text" : "This is about us",
+        "my_number" : 123, 
+        "my_list" : [ "one", "two", "three", 4 ]
+    }
+    return render(request, "about.html", my_context)
